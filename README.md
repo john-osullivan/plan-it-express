09805bc89aa04a0d5370d9c8fc728dfefa9306f0
John O'Sullivan <johno@mit.edu>
2015-04-08 20:32:10 -0400

    Did a pretty thorough overhaul.  Got rid of node.js, set up a single-page-app structure, and wrote a menu.

--- README.md
+++
@@ -1,2 +0,0 @@
-# plan-it-express
-The user interface for an event planning tool, as developed in the Spring 2015 offering 6.813 by David Bocek, Christian Infante, John O'Sullivan, and Zach Sather.
---
+++ plan-it.js
@@ -0,0 +1,42 @@
+var isActive = {'home':true, 'announcements':false, 'questions':false, 'notes':false, 'todo':false};
+
+var loadPage = function(renderedTemplate){
+    $('#container').html(renderedTemplate);
+};
+
+var setActive = function(page){
+    for (var entry in isActive){
+        isActive[entry] = page === entry;
+    }
+};
+
+var loadHome = function(){
+    setActive('home');
+    var newMenu = nunjucks.render('menu.html');
+    var home = nunjucks.render('home.html');
+    loadPage(home);
+};
+
+var loadAnnouncements = function(){
+    var announcements = nunjucks.render('announcements.html');
+    setActive('announcements');
+    loadPage(announcements);
+};
+
+var loadQuestions = function(){
+    var questions = nunjucks.render('questions.html');
+    setActive('questions');
+    loadPage(questions);
+};
+
+var loadNotes = function(){
+    var notes = nunjucks.render('notes.html');
+    setActive('notes');
+    loadPage(notes);
+};
+
+var loadTodos = function(){
+    var todos = nunjucks.render('todos.html');
+    setActive('todo');
+    loadPage(todos);
+};
